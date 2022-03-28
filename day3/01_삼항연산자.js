0e208885d067213a749a3f023065841e08e85fe5
grape420 <grape420@naver.com>
2022-03-28 19:49:22 +0900

    20220328 19:49 삼항연산자와 if문

---
+++ day3/01_삼항연산자.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let a = 10, b = 40, result = '';
+
+result = ( a > b ) ? 'a가 b보다 크다' : 'a가 b보다 작다';
+console.log(result);
+
+/*
+  삼항연산자 
+
+  조건문 ? 참결과 : 거짓결과
+  관계, 논리 - true / false + 산술 
+*/
