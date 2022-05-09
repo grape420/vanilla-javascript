2fd99e28292c8be786c11dbc8d6e6df0aee8eeaf
grape420 <grape420@naver.com>
2022-05-09 19:05:50 +0900

    20220509 19:04

---
+++ day15/js/ex10.js
@@ -0,0 +1,8 @@
+;(function(){
+    const get = target => document.querySelector( target );
+    const getAll = target => document.querySelectorAll( target );
+
+    $img  = get('.banner .big-img img');
+
+
+})()
